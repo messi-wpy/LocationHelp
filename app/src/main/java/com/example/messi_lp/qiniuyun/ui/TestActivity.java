bbfbb3b648347e273687d8ca82048de1c7a829cc
messi-wpy <904315105@qq.com>
2018-08-24 16:44:31 +0800

    第一次

---
+++ app/src/main/java/com/example/messi_lp/qiniuyun/ui/TestActivity.java
@@ -0,0 +1,15 @@
+package com.example.messi_lp.qiniuyun.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TestActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle saveOnInstanceState) {
+
+        super.onCreate(saveOnInstanceState);
+
+    }
+
+}
